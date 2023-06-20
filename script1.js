dabecadc8012e27ea0367c60cd70291aaf51ca6d
MauPos04 <mauriciop1palma@gmail.com>
2023-06-19 20:11:00 -0500

    Initial commit

---
+++ script1.js
@@ -0,0 +1,18 @@
+function showAlert() { alert('¡Hola, esta es una alerta desde JavaScript!');}
+
+
+
+
+//Obetenemos el boton por su id
+let boton = document.getElementById("miBoton");
+
+//Añadimos un oyente de eventos que "escuche el evento de clicl"
+
+boton.addEventListener("click", function(){
+    alert("¡Haz hecho clic en el botón!");
+});
+
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+           document.querySelector(".nav-links").
+                      classList.toggle("nav-links-responsive")})
